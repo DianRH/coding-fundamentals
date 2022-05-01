124e5e8e367efd66d014e8a24949393b7ba5be55
Diana <diana__rh@hotmail.com>
2022-04-30 23:33:31 -0500

    promedio examenes con for

--- PromedioExamenes/PromedioExamenes.js
+++ PromedioExamenes/PromedioExamenes.js
@@ -5,19 +5,39 @@ Al final del programa nos tiene que devolver el promedio de las notas.
 
 Condiciones( El usuario no puede ingresar notas negativas, ni mayores a 10 ) */
 
+/* ----- while ----- */
+// definir variables
+ // var cantidadExamenes = parseInt(prompt("Ingrese el número de examenes:"));
+ // var suma = 0
+ // var examen = 0
+ //
+ // while (examen<cantidadExamenes) {
+ //   var nota = parseInt(prompt(`Ingrese nota ${examen+1} de ${cantidadExamenes}`));
+ //   if (nota>=0 && nota<=10) {
+ //     suma += nota
+ //     examen++
+ //   } else {
+ //     alert(`Usted ingreso ${nota} debe ingresar un número del 1 al 10`)
+ //   }
+ // }
+
+ // var promedio = suma/cantidadExamenes
+ // alert(`Ingreso ${cantidadExamenes} notas y el promedio es ${promedio.toFixed(2)}`)
+
+
+/* ----- for ----- */
 // definir variables
  var cantidadExamenes = parseInt(prompt("Ingrese el número de examenes:"));
  var suma = 0
- var examen = 0
 
- while (examen<cantidadExamenes) {
+ for(var examen =0 ;examen<cantidadExamenes ;examen++){
    var nota = parseInt(prompt(`Ingrese nota ${examen+1} de ${cantidadExamenes}`));
    if (nota>=0 && nota<=10) {
      suma += nota
-     examen++
-   } else {
-     alert(`Usted ingreso ${nota} debe ingresar un número del 1 al 10`)
-   }
+ }else {
+   alert(`Usted ingreso ${nota} debe ingresar un número del 1 al 10`)
  }
+}
+
  var promedio = suma/cantidadExamenes
  alert(`Ingreso ${cantidadExamenes} notas y el promedio es ${promedio.toFixed(2)}`)
